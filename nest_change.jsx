36a7d8681637dac7dc04420cddd3cac9ffac764d
yu-mitsubayashi <32918094+yu-mitsubayashi@users.noreply.github.com>
2017-10-19 13:03:50 +0900

    test
    test script

---
+++ nest_change.jsx
@@ -0,0 +1,42 @@
+﻿var selectedComp_count = new Array();
+var selectedComp = new Array();
+
+for (var i=1; i<=app.project.items.length; i++)//count  selected  composition
+{  
+    if(app.project.items[i] instanceof CompItem)
+    {
+        if(app.project.items[i].selected == true)
+        {
+            selectedComp_count.push(app.project.items[i])
+        }
+    }
+}
+
+app.beginUndoGroup("precomp")
+
+if (selectedComp_count == 0)//judge selected  composition count
+{
+    for (var i=1; i<=app.project.items.length; i++)//Change all comp
+    {  
+        if(app.project.items[i] instanceof CompItem)//All items are composition or not 
+        {
+            app.project.items[i].preserveNestedFrameRate =  true;//All compositions change parameter
+        }
+    }
+    alert("[All comp] Nested on!")
+}
+else
+{
+    for (var i=1; i<=app.project.items.length; i++)//change Selected comp
+    {  
+        if(app.project.items[i] instanceof CompItem)//All items are composition or not
+        {
+            if(app.project.items[i].selected == true)//"CompItem" is selected or not 
+            {
+                app.project.items[i].preserveNestedFrameRate =  true;//Appropriate compositions change parameter
+            }
+        }
+    }
+    alert("[Selected comp] Nested on!")
+}
+
---
+++ workarea_reset.jsx
@@ -0,0 +1,44 @@
+﻿var selectedComp_count = new Array();
+var selectedComp = new Array();
+
+for (var i=1; i<=app.project.items.length; i++)//count 
+{  
+    if(app.project.items[i] instanceof CompItem)
+    {
+        if(app.project.items[i].selected == true)
+        {
+            selectedComp_count.push(app.project.items[i])
+        }
+    }
+}
+
+app.beginUndoGroup("precomp")
+
+if (selectedComp_count == 0)
+{
+    for (var i=1; i<=app.project.items.length; i++)//コンポの尺とワークエリアを調整
+    {  
+        if(app.project.items[i] instanceof CompItem)
+        {
+            app.project.items[i].workAreaStart =  0;//ワークエリアのスタートを指定
+            app.project.items[i].workAreaDuration =  app.project.items[i].duration - app.project.items[i].workAreaStart; //ワークエリアのエンドを指定
+        }
+    }
+    alert("[All comp] workarea reseted!")
+}
+else
+{
+    for (var i=1; i<=app.project.items.length; i++)//コンポの尺とワークエリアを調整
+    {  
+        if(app.project.items[i] instanceof CompItem)
+        {
+            if(app.project.items[i].selected == true)
+            {
+                app.project.items[i].workAreaStart =  0;//ワークエリアのスタートを指定
+                app.project.items[i].workAreaDuration =  app.project.items[i].duration - app.project.items[i].workAreaStart; //ワークエリアのエンドを指定
+            }
+        }
+    }
+    alert("[Selected comp] workarea reseted!")
+}
+
